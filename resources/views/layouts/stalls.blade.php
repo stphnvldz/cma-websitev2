334a330c6f156af8af659439ea918589a55d197e
dandrecollera <52880769+dandrecollera@users.noreply.github.com>
2023-03-10 04:49:32 +0800

    remove href

--- resources/views/layouts/stalls.blade.php
+++ resources/views/layouts/stalls.blade.php
@@ -134,7 +134,7 @@
           </script>
         </div>
         <div class="d-grid gap-1 d-md-flex justify-content-md-end">
-           <a href="/tenants" class="btn btn-primary" id="#proceed-btn">Proceed</a>
+           <a class="btn btn-primary" id="#proceed-btn">Proceed</a>
         </div>
     </div>
 </form>
