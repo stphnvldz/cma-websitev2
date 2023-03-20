f39e5bb7b947c2001cb751d5147c098d5583d288
stphnvldz <valdezstephanieeee@gmail.com>
2023-03-20 10:42:04 +0800

    main page

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -9,20 +9,31 @@
     <link rel="stylesheet" href="/assets/css/bootstrap.css">
 </head>
 <body>
-    <div class="container">
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-        <a href="/" class="navbar-brand d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-            <img src="/assets/img/cma_logo.png" alt="" style="max-width: 6rem">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
+        <div class="container-fluid">
+          <img src="/assets/img/cma_logo.png" alt="" style="width:100px;height:50px;">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-3">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                  </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/about">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/requirements">Requirements</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/location">Location</a>
+              </li>
+             
+          </div>
+        </div>
+      </nav>
 
-        </a>
-    
-        <ul class="nav nav-pills my-auto">
-            <li class="nav-item"><a href="/login" class="nav-link active me-2" aria-current="page">Login</a></li>
-            <li class="nav-item"><a href="/register" class="nav-link active" aria-current="page">Register</a></li>
-            
-        </ul>
-        </header>
-    </div>
 
     <script src="assets/js/bootstrap.js"></script>
 </body>
