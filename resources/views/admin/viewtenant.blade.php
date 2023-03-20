22b1fa1597ff7ac2c322906366835cf76cff2952
zeesaa <lozadazeesa@gmail.com>
2023-03-20 11:20:44 +0800

    edit button

--- resources/views/admin/viewtenant.blade.php
+++ resources/views/admin/viewtenant.blade.php
@@ -37,49 +37,49 @@
       <div class="mb-3 row">
         <label for="inputName4" class="col-sm-3 col-form-label align-self-center">Full Name</label>
         <div class="col-sm-9">
-          <input type="fullname" name="fullname" class="form-control" id="fullname" readonly value="{{ $data->fullname }}">
+          <input type="fullname" name="fullname" class="form-control" id="fullname" disabled value="{{ $data->fullname }}">
         </div>
       </div>
       <div class="mb-3 row">
         <label for="inputBirthday" class="col-sm-3 col-form-label align-self-center">Date of Birth</label>
         <div class="col-sm-9">
-          <input type="dateofbirth" name="dateofbirth" class="form-control" id="dateofbirth" readonly value="{{ $data->dateofbirth }}">
+          <input type="dateofbirth" name="dateofbirth" class="form-control" id="dateofbirth" disabled value="{{ $data->dateofbirth }}">
         </div>
       </div>
       <div class="mb-3 row">
         <label for="inputAddress" class="col-sm-3 col-form-label align-self-center">Address</label>
         <div class="col-sm-9">
-          <input type="address" name="address" class="form-control" id="address" style="height: 100px;"  value="{{ $data->address }}" readonly>
+          <input type="address" name="address" class="form-control" id="address" style="height: 100px;"  value="{{ $data->address }}" disabled>
         </div>
       </div>
       <div class="mb-3 row">
         <label for="inputcontact4" class="col-sm-3 col-form-label align-self-center">Contact No.</label>
         <div class="col-sm-9">
-          <input type="contact" name="contact" id="contact" class="form-control" value="{{ $data->contact }}" readonly>
+          <input type="contact" name="contact" id="contact" class="form-control" value="{{ $data->contact }}" disabled>
         </div>
       </div>
       <div class="mb-3 row">
         <label for="inputEmail4" class="col-sm-3 col-form-label align-self-center">Email Address</label>
         <div class="col-sm-9">
-          <input type="emailadd" name="emailadd" id="emailadd" class="form-control" value="{{ $data->emailadd }}" readonly>
+          <input type="emailadd" name="emailadd" id="emailadd" class="form-control" value="{{ $data->emailadd }}" disabled>
         </div>
       </div>
       <div class="mb-3 row">
         <label for="stallnum" class="col-sm-3 col-form-label align-self-center">Stall Number</label>
         <div class="col-sm-9">
-          <input type="text" name="stallnum" class="form-control" id="stallnum" value="{{ $data->stallnum }}" readonly>
+          <input type="text" name="stallnum" class="form-control" id="stallnum" value="{{ $data->stallnum }}" disabled>
         </div>
       </div>
       <div class="mb-3 row">
         <label for="stallname" class="col-sm-3 col-form-label align-self-center">Stall Name</label>
         <div class="col-sm-9">
-          <input id="stallname" name="stallname" class="form-control" value="{{ $data->stallname }}" readonly>
+          <input id="stallname" name="stallname" class="form-control" value="{{ $data->stallname }}" disabled>
         </div>
       </div>
       <div class="mb-3 row">
         <label for="stalltype" class="col-sm-3 col-form-label align-self-center">Stall Type</label>
         <div class="col-sm-9">
-          <select id="stalltype" name="stalltype" class="form-control" readonly>
+          <select id="stalltype" name="stalltype" class="form-control" disabled>
             <option value="Regular" {{ $data->stalltype == 'Regular' ? 'selected' : '' }}>Regular</option>
             <option value="Temporary" {{ $data->stalltype == 'Temporary' ? 'selected' : '' }}>Temporary</option>
         </select>
@@ -88,7 +88,7 @@
       <div class="mb-3 row">
         <label for="payment" class="col-sm-3 col-form-label align-self-center">Choice of Payment</label>
         <div class="col-sm-9 d-flex align-items-center">
-          <input id="payment" name="payment" class="form-control" value="{{ $data->payment }}" readonly>
+          <input id="payment" name="payment" class="form-control" value="{{ $data->payment }}" disabled>
         </div>
       </div>
       <div class="mb-3 row">
@@ -104,7 +104,7 @@
       <button type="button" class="btn btn-primary" id="edit">Edit Information</button>
     </div>
     <div class="text-center md-2 mx-2">
-      <button type="submit" class="btn btn-primary">Save</button>
+      <button type="submit" id="save"class="btn btn-primary"disabled>Save</button>
     </div>
   </div>
 </form>
@@ -122,18 +122,20 @@
     var stallnameInput = document.getElementById("stallname");
     var stalltypeInput = document.getElementById("stalltype");
     var paymentInput = document.getElementById("payment");
+    var saveButton = document.getElementById("save");
     // Add an event listener to the edit button
     editButton.addEventListener("click", function() {
       // Enable the input fields
-      fullnameInput.removeAttribute("readonly");
-      dateofbirthInput.removeAttribute("readonly");
-      addressInput.removeAttribute("readonly");
-      contactInput.removeAttribute("readonly");
-      emailaddInput.removeAttribute("readonly");
-      stallnumInput.removeAttribute("readonly");
-      stallnameInput.removeAttribute("readonly");
+      fullnameInput.removeAttribute("disabled");
+      dateofbirthInput.removeAttribute("disabled");
+      addressInput.removeAttribute("disabled");
+      contactInput.removeAttribute("disabled");
+      emailaddInput.removeAttribute("disabled");
+      stallnumInput.removeAttribute("disabled");
+      stallnameInput.removeAttribute("disabled");
       stalltypeInput.removeAttribute("disabled");
-      paymentInput.removeAttribute("readonly");
+      paymentInput.removeAttribute("disabled");
+      saveButton.removeAttribute("disabled");
     });
     </script>
 
