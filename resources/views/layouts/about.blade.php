ce12013c83ee4a4061ae6c5c1244dc850e46842d
stphnvldz <valdezstephanieeee@gmail.com>
2023-04-12 14:10:48 +0800

    publicview

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -12,8 +12,7 @@
     <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
         <div class="container-fluid">
           <img src="/assets/img/cma_logo.png" alt="" style="width:100px;height:50px;">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+          
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-3">
@@ -33,7 +32,54 @@
           </div>
         </div>
       </nav>
+      
+      <div style="margin-bottom: 50px; font-family: Arial, sans-serif mt-5; font-size: 50px; font-weight: bold; color: #333; text-align: center;">
+        CITY MALL OF ANTIPOLO
+      </div>
+     
+     
+      <div>
+        <div style="float: left; margin-right: 10px; margin-left: 10px; margin-top: 10px;">
+          <img src="assets/img/cma2.png" style="width:400px;height:300px;">
+        </div>
+        <div style="margin-top: 5px; margin-left: 10px; padding: 10px; font-size: 20.7px; background-color: #ffffff;">
+          <p>City Mall of Antipolo, as this place is one of the biggest public markets
+            in Antipolo City that can provide all the basic needs for its customer. 
+            The City Mall of Antipolo is ranked number 21 in the top-rated shopping center 
+            in Antipolo, they are also rank number 8 in the most reviewed shopping center in Antipolo.  
+           </p>
+           <p> It consists of 3 floors that contains wet and dry, groceries and other amenities. the wet and dry section
+            is located in the first floor of the market, the groceries and other necessities is located at the second floor
+            and the third floor contains other amenities like dental clinic, jewelry shops etc. 
+            This public market is very useful for the citizens of Antipolo City because people can find almost 
+            all the things that they need in this place.</p>
+        </div>
+      </div>
 
+      <div>  
+        <div style="float: right; margin-left: 10px; margin-right: 100px;">
+          <img src="assets/img/hotline.png" style="width:500px;height:500px;">
+        </div>
+        <div class="container px-4 mt-2" style="font-size: 17px; float: left; margin-right: 20px; margin-left: 100px; width:550px;height:455px;border: 1px solid rgb(39, 38, 38);border-radius: 4px;">
+          <p style="font-weight: bold;">CITY HALL - ANNEX III
+            SERVICES OFFERED:</p>
+            <p>MEDICAL CONSULTATIONS</p>
+            <p>DENTAL SERVICES</p>
+            <p>DAYCARE SERVICE</p>
+            <p>BIRTH CERTIFICATE
+            - Application of New Born and Late Registration</p>
+            <p>SENIOR CITIZENS
+            - ID Applications, Issuance of Purchase Booklet
+            <p>APPLICATION FOR PERSONS WITH DISABILITY (PWDs) ID</p>
+              <p>APPLICATION FOR SOLO PARENT ID</p>
+                <p>ASSISTANCE IN CRISIS SITUATION</p>
+                  <p>REFERRAL TO PUBLIC EMPLOYMENT SERVICE OFFICE
+            (PESO) FOR LOCAL AND OVERSEAS JOB HIRING</p>
+       
+        </div>
+
+         
+ 
 
     <script src="assets/js/bootstrap.js"></script>
 </body>
--- resources/views/layouts/requirements.blade.php
+++ resources/views/layouts/requirements.blade.php
@@ -35,6 +35,31 @@
         </div>
       </nav>
 
+      <div style=" margin-bottom: 50px; font-family: Arial, sans-serif mt-5; font-size: 50px; font-weight: bold; color: #333; text-align: center;">
+        TENANT APPLICATION REQUIREMENTS
+      </div>
+
+      <div>  
+        <div style="float: right; margin-left: 10px; margin-right: 100px;">
+          <img src="assets/img/cma3.png" style="width:500px;height:425px;">
+          <p style="font-weight: bold; margin-top: 15px;">Go to the office of City Mall of Antipolo located in the third floor of the Market.</p>
+        </div>
+        <div class="container px-4 mt-2" style="font-size: 20px; float: left; margin-right: 20px; margin-left: 100px; width:550px;height:455px;border: 1px solid rgb(39, 38, 38);border-radius: 4px;">
+          <p style="font-weight: bold; margin-top: 15px;">REQUIREMENTS IN AVAILING/RENTING A STALL:</p>
+            <p>NBI CLEARANCE or POLICE CLEARANCE</p>
+            <p>BRGY CLEARANCE</p>
+            <p>VALID GOVERNMENT ID</p>
+            <p style=" margin-top: 15px; ">NOTES:</p>
+            <p>1. 
+              Interested Applicants must be residing in Antipolo City ONLY.</p>
+            <p>2. Applicants must have no derogatory remarks in their NBI Clearance or 
+              any criminal records; therefore has a good morale.</p>
+            
+       
+        </div>
+
+
+
     <script src="assets/js/bootstrap.js"></script>
 </body>
 </html>
