ebb7d16741e582ec53c428facc8ae334e36bb94e
zeesaa <lozadazeesa@gmail.com>
2023-04-19 19:16:12 +0800

    table tenant

--- database/migrations/2023_02_26_083959_create_tenant_table.php
+++ database/migrations/2023_02_26_083959_create_tenant_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('address');
             $table->string('contact');
             $table->string('emailadd');
-            $table->string('stallprice');
             $table->mediumText('image')->nullable();
             $table->timestamps();
         });
