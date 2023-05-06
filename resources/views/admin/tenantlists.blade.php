05080bfdde05da5939de2f02aee4a532c516603f
zeesaa <lozadazeesa@gmail.com>
2023-05-06 21:22:53 +0800

    cmaweb

--- resources/views/admin/tenantlists.blade.php
+++ resources/views/admin/tenantlists.blade.php
@@ -47,6 +47,66 @@
         @endif
         </tbody>
     </table>
+    <div class="modal fade" id="billModal" tabindex="-1" aria-labelledby="billModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-fullscreen-md-down">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="cash-modal">Bill Tenant</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <div class="d-flex justify-content-center row">
+                <div class="mb-3 row text-center">
+                    <div class="mb-3 row">
+                        <label for="inputName4" class="col-sm-3 col-form-label align-self-center">Full Name</label>
+                        <div class="col-sm-9">
+                            <input type="text" name="fullname" class="form-control" id="fullname"  readonly>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="inputName4" class="col-sm-3 col-form-label align-self-center">Contact</label>
+                        <div class="col-sm-9">
+                            <input type="text" name="contact" class="form-control" id="contact"readonly>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                      <label for="inputName4" class="col-sm-3 col-form-label align-self-center">Email</label>
+                      <div class="col-sm-9">
+                          <input type="text" name="email" class="form-control" id="email"readonly>
+                      </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="inputName4" class="col-sm-3 col-form-label align-self-center">Total Amount</label>
+                        <div class="col-sm-9">
+                            <input type="text" name="amount" class="form-control" id="amount"readonly>
+                        </div>
+                      </div>
+                    <div class="mb-3 row">
+                        <label for="inputName4" class="col-sm-3 col-form-label align-self-center">Notice</label>
+                        <div class="col-sm-9">
+                          <input type="text" name="notice" class="form-control" id="notice">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="inputName4" class="col-sm-3 col-form-label align-self-center">Description</label>
+                        <div class="col-sm-9">
+                          <input type="text" name="description" class="form-control" id="description">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="inputName4" class="col-sm-3 col-form-label align-self-center">Date</label>
+                        <div class="col-sm-9">
+                          <input type="text" name="date" class="form-control" id="date">
+                        </div>
+                    </div>
+                </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Confirm</button>
+            </div>
+          </div>
+        </div>
+      </div>
 
 </body>
 </html>
