15ecfe4b072e2d2579b89ba18e250b96d20ab2c2
stphnvldz <valdezstephanieeee@gmail.com>
2023-05-11 12:47:43 +0800

    publicview

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -9,78 +9,73 @@
     <link rel="stylesheet" href="/assets/css/bootstrap.css">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
-        <div class="container-fluid">
-          <img src="/assets/img/cma_logo.png" alt="" style="width:100px;height:50px;">
-          
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-3">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">Home</a>
-                  </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/about">About</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/requirements">Requirements</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/location">Location</a>
-              </li>
-             
-          </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="/assets/img/cma_logo.png" alt="" style="width:100px;height:50px;">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/requirements">Requirements</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/location">Location</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-      </nav>
+    </nav>
       
       <div style="margin-bottom: 50px;margin-top: 50px; font-family: Arial, sans-serif; font-size: 50px; font-weight: bold; color: #333; text-align: center;">
         CITY MALL OF ANTIPOLO
       </div>
      
      
-      <div>
-        <div style="float: left; margin-right: 10px; margin-left: 100px; margin-top: 10px;">
-          <img src="assets/img/cma2.png" style="width:400px;height:300px;">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6" style="margin-top: 10px;">
+            <img src="assets/img/cma2.png" class="img-fluid" alt="City Mall of Antipolo" style="max-width: 100%;">
+          </div>
+          <div class="col-md-6" style="margin-top: 10px;">
+            <div style="padding: 10px; font-size: 20.7px; background-color: #ffffff;">
+              <p>City Mall of Antipolo is one of the biggest public markets in Antipolo City that provides all the basic needs for its customers. It is ranked number 21 in the top-rated shopping centers in Antipolo and number 8 in the most reviewed shopping centers in Antipolo.</p>
+              <p>It consists of 3 floors that contain wet and dry markets, groceries, and other amenities. The wet and dry section is located on the first floor, groceries and other necessities on the second floor, and the third floor contains amenities like a dental clinic and jewelry shops. This public market is very useful for the citizens of Antipolo City as they can find almost all the things they need in one place.</p>
+            </div>
+          </div>
         </div>
-        <div style="margin-top: 5px; margin-left: 10px; padding: 10px; font-size: 20.7px; background-color: #ffffff;">
-          <p>City Mall of Antipolo, as this place is one of the biggest public markets
-            in Antipolo City that can provide all the basic needs for its customer. 
-            The City Mall of Antipolo is ranked number 21 in the top-rated shopping center 
-            in Antipolo, they are also rank number 8 in the most reviewed shopping center in Antipolo.  
-           </p>
-           <p> It consists of 3 floors that contains wet and dry, groceries and other amenities. the wet and dry section
-            is located in the first floor of the market, the groceries and other necessities is located at the second floor
-            and the third floor contains other amenities like dental clinic, jewelry shops etc. 
-            This public market is very useful for the citizens of Antipolo City because people can find almost 
-            all the things that they need in this place.</p>
-        </div>
+        
+        <div class="row" style="margin-top: 50px;">
+          <div class="col-md-6" style="margin-top: 10px;">
+              <img src="assets/img/hotline.png" class="img-fluid" alt="Hotline" style="max-width: 100%;">
+          </div>
+          <div class="col-md-6 mt-4" style="font-size: 17px;">
+              <div class="container px-4" style="border: 1px solid rgb(39, 38, 38); border-radius: 4px;">
+                  <p style="font-weight: bold;">CITY HALL - ANNEX III SERVICES OFFERED:</p>
+                  <p>MEDICAL CONSULTATIONS</p>
+                  <p>DENTAL SERVICES</p>
+                  <p>DAYCARE SERVICE</p>
+                  <p>BIRTH CERTIFICATE - Application of New Born and Late Registration</p>
+                  <p>SENIOR CITIZENS - ID Applications, Issuance of Purchase Booklet</p>
+                  <p>APPLICATION FOR PERSONS WITH DISABILITY (PWDs) ID</p>
+                  <p>APPLICATION FOR SOLO PARENT ID</p>
+                  <p>ASSISTANCE IN CRISIS SITUATION</p>
+                  <p>REFERRAL TO PUBLIC EMPLOYMENT SERVICE OFFICE (PESO) FOR LOCAL AND OVERSEAS JOB HIRING</p>
+              </div>
+          </div>
       </div>
-
-      <div>  
-        <div style="float: right; margin-left: 10px; margin-right: 100px; margin-top: 10px;">
-          <img src="assets/img/hotline.png" style="width:500px;height:500px;">
-        </div>
-        <div class="container px-4 mt-4" style="font-size: 17px; float: left; margin-right: 20px; margin-left: 100px; width:550px;height:455px;border: 1px solid rgb(39, 38, 38);border-radius: 4px;">
-          <p style="font-weight: bold;">CITY HALL - ANNEX III
-            SERVICES OFFERED:</p>
-            <p>MEDICAL CONSULTATIONS</p>
-            <p>DENTAL SERVICES</p>
-            <p>DAYCARE SERVICE</p>
-            <p>BIRTH CERTIFICATE
-            - Application of New Born and Late Registration</p>
-            <p>SENIOR CITIZENS
-            - ID Applications, Issuance of Purchase Booklet
-            <p>APPLICATION FOR PERSONS WITH DISABILITY (PWDs) ID</p>
-              <p>APPLICATION FOR SOLO PARENT ID</p>
-                <p>ASSISTANCE IN CRISIS SITUATION</p>
-                  <p>REFERRAL TO PUBLIC EMPLOYMENT SERVICE OFFICE
-            (PESO) FOR LOCAL AND OVERSEAS JOB HIRING</p>
-        </div>
-      </div>
-
-         
- 
-
+          
     <script src="assets/js/bootstrap.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
