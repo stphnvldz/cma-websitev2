4df03adf71f2f10406d4e0e2aa15622c566397db
zeesaa <lozadazeesa@gmail.com>
2023-05-15 01:08:35 +0800

    payment

--- resources/views/admin/repors/paymentreports.blade.php
+++ resources/views/admin/repors/paymentreports.blade.php
@@ -20,7 +20,6 @@
         <a class="printPage mt-3" href="#">Print</a>
     </div>
     
-<<<<<<< HEAD
 <div class="payment-table">
     <table class="table table-hover table-striped" id="rentstall">
         <thead>
@@ -60,49 +59,6 @@
     </table>
 </div>
 
-=======
-    <div class="payment-table">
-        <div class="table-responsive">
-            <table class="table table-hover table-striped" id="printTable">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Full Name</th>
-                        <th scope="col">Stall Number</th>
-                        <th scope="col">Rental Fee Type</th>
-                        <th scope="col">Amount</th>
-                        <th scope="col">Date From</th>
-                        <th scope="col">Date To</th>
-                        <th scope="col">Mode of Payment</th>
-                        <th scope="col">Reference Number</th>
-                        <th scope="col">Status</th>
-                        <th scopre="col">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @if(isset($payment))
-                    @foreach($payment as $payments)
-                    <tr>
-                        <td>{{$payments->id}}</td>
-                        <td>{{ $payments->fullname }}</td>
-                        <td>{{ $payments->stallnumber }}</td>
-                        <td>{{ $payments->type }}</td>
-                        <td>{{ $payments->amount }}</td>
-                        <td>{{ $payments->datefrom }}</td>
-                        <td>{{ $payments->dateto }}</td>
-                        <td>{{ $payments->payment }}</td>
-                        <td>{{ $payments->refnumber }}</td>
-                        <td>{{ $payments->status }}</td>
-                        <td><button type="button" class="btn btn-success view-button" data-bs-toggle="modal" data-bs-target="#viewModal" data-row-id="{{ $payments->id }}">View</button></td>
-                    </tr>
-                    @endforeach
-                    @endif
-                </tbody>
-            </table>
-        </div>
-    </div>
-    
->>>>>>> 7dccb7daf8b35ef30aecabbf3c5a1eb75e5e8926
     <div class="modal fade" id="viewModal" tabindex="-1" aria-labelledby="viewModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-lg">
             <div class="modal-content">
